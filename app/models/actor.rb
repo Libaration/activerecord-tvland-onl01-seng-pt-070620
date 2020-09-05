83c23de09591eea781093d43de0e83fed86c0bca
Cristian Deleon <libaration@gmail.com>
2020-09-05 01:08:31 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+  has_many(:character)
+  has_many(:show, :through => :character)
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def characters
+    self.character
+  end
+
+  def list_roles
+    self.character.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
-  
+  has_many(:characters)
+  has_many(:actors, :through => :characters)
+  belongs_to(:network)
+
+  def actors_list
+    self.actors.collect do |actor|
+      actor.full_name
+    end
+  end
+
+  def build_network(opts = {})
+    self.network = Network.find_or_create_by(opts.keys.first => opts[opts.keys.first])
+  end
+
 end
